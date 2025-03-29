a9f9ab1097dd7d94bbd177c11b6cfdec57e31cc8
Jaitrika <141649577+Jaitrika@users.noreply.github.com>
2025-03-29 19:22:39 +0530

    Update and rename celebrities.ts to c.ts

---
+++ c.ts
@@ -0,0 +1,88 @@
+export const celebrities = {
+  Bradley_Cooper: {
+    image: "https://preview.redd.it/bradley-cooper-v0-1jcb9f6i8doa1.jpg?width=800&format=pjpg&auto=webp&s=d42b76351b7870dc3bb8f11ca3c0f77dfe385354",
+    description: "Bradley Cooper is an American actor and filmmaker known for his roles in The Hangover, Silver Linings Playbook, American Sniper, and A Star Is Born, which he also directed. He has won a BAFTA and three Grammy Awards and has been nominated for multiple Academy Awards and Golden Globes. Cooper started with TV roles in Alias and Sex and the City before rising to fame. He is one of Hollywood’s highest-paid actors, and his films have grossed billions worldwide.",
+  },
+  Jennifer_Aniston: {
+    image: "https://preview.redd.it/jennifer-aniston-in-season-1-of-friends-v0-dion0e42nqga1.jpg?width=564&format=pjpg&auto=webp&s=7e9a9d1ae547f15343aeef9e8a58769159fb1634",
+    description: "Jennifer Aniston is an American actress best known for playing Rachel Green in Friends, a role that earned her an Emmy, a Golden Globe, and a Screen Actors Guild Award. She has starred in hit comedies like Marley & Me, Horrible Bosses, and Just Go with It, as well as critically acclaimed films like Cake and The Good Girl. She later returned to TV with The Morning Show, winning another Screen Actors Guild Award.",
+  },
+  Andrew_Garfield: {
+    image: "https://preview.redd.it/happy-40th-birthday-to-andrew-garfield-v0-8y8amzvofajb1.jpg?width=2000&format=pjpg&auto=webp&s=23be48b2c1d0a20a8a92d1c0e01b9a1b16e3e099",
+    description: "Andrew Garfield is a British-American actor best known for playing Spider-Man in The Amazing Spider-Man films and Spider-Man: No Way Home. He earned Oscar nominations for Hacksaw Ridge and Tick, Tick... Boom! and won a Golden Globe. His other notable works include The Social Network, Silence, and the TV series Under the Banner of Heaven. He has also won a Tony Award for his stage performances.",
+  },
+  Leonardo_DiCaprio: {
+    image: "https://preview.redd.it/what-is-it-about-leonardo-dicaprios-features-that-made-him-v0-zjgzgv9tcdt81.jpg?width=599&format=pjpg&auto=webp&s=090e828d1872ec2b6948c9d0b167fdd6bf6eb851",
+    description: "Full Name: Leonardo Wilhelm DiCaprio ;Date of Birth: November 11, 1974 ;Place of Birth: Los Angeles, California, USA ;Nationality: American ;Profession: Actor, Film Producer; Famous For: Titanic, Inception, The Wolf of Wall Street, The Revenant ;Awards: Academy Award, BAFTA, Golden Globe Awards ; Notable Collaborations: Martin Scorsese, Christopher Nolan, Quentin Tarantino ;Debut Film: Critters 3 (1991); Breakthrough Role: What's Eating Gilbert Grape (1993) ; Highest-Grossing Film: Titanic (1997)",
+  },
+  Julia_Roberts: {
+    image: "https://preview.redd.it/julia-roberts-v0-vgak1yz5cbhb1.jpg?width=2560&format=pjpg&auto=webp&s=d4649349ce5db405067dc8c6b212835c5c02dbd6",
+    description: "Julia Roberts is an American actress known for her leading roles in romantic comedies like Pretty Woman, My Best Friend’s Wedding, and Notting Hill. She won an Academy Award for Erin Brockovich and has starred in hits like Ocean's Eleven, Eat Pray Love, and Wonder. One of Hollywood’s most bankable stars, she was nicknamed America’s Sweetheart for her charisma and widespread popularity.",
+  },
+  Marilyn_Monroe: {
+    image: "https://preview.redd.it/marilyn-monroe-v0-6s4n0w8tzu9a1.jpg?width=2560&format=pjpg&auto=webp&s=dd1018b455662767c593444415516fefc7d30dee",
+    description: "Marilyn Monroe, born Norma Jeane Mortenson on June 1, 1926, in Los Angeles, was an American actress, model, and singer, known for films like *Gentlemen Prefer Blondes*, *Some Like It Hot*, and *The Seven Year Itch*; she became a 1950s Hollywood icon and won a Golden Globe for *Some Like It Hot*, but faced studio disputes, a nude photo scandal, and rumored ties to John F. Kennedy before her untimely death on August 4, 1962.",
+  },
+  Scarlett_Johansson: {
+    image: "https://preview.redd.it/what-people-in-this-sub-have-against-scarlett-johansson-of-v0-72ai7uz1trbd1.jpg?width=720&format=pjpg&auto=webp&s=9bcf7796afbd3c3f9e18febcfc0e64acaacc9d64",
+    description: "Scarlett Johansson is an American actress and singer known for playing Black Widow in the Marvel Cinematic Universe. She has starred in Lost in Translation, Marriage Story, Jojo Rabbit, and Lucy. The world's highest-paid actress in 2018 and 2019, her films have grossed over $15 billion worldwide. She has won a BAFTA and a Tony Award and was named one of Time’s most influential people.",
+  }
+  ,
+  Will_Smith: {
+    image: "https://preview.redd.it/will-smith-v0-crdmsjjo54q81.jpg?auto=webp&s=bdd99ddbea7b7f310678970f694de1d277e220e3",
+    description: "Will Smith is an American actor, rapper, and film producer. He first gained fame as a rapper before starring in The Fresh Prince of Bel-Air. He became a Hollywood star with Men in Black, Independence Day, Bad Boys, and I Am Legend. He won an Academy Award for King Richard and has also received multiple Grammys for his music. With a box office gross exceeding $9.5 billion, he is one of the industry's biggest stars.",
+  }
+  ,
+  Taylor_Swift: {
+    image: "https://external-preview.redd.it/why-swifties-think-taylor-swift-is-planning-something-big-v0-A5MARCHNBrYcJgXwrrmm9xq9jgQwYRHa40SVvSCuYJs.jpg?auto=webp&s=8a2004c4ebfa08471ac0efcdb413fe36d59c936c",
+    description: "no desc for now",
+  },
+
+  Billie_Eilish: {
+    image: "https://i.redd.it/what-are-your-thoughts-on-billie-eilish-v0-u4ypkyxj9fmc1.jpg?width=3000&format=pjpg&auto=webp&s=8e67afa25ab1c3cd29c1f705cc110bafc9a77245",
+    description: "no desc for now",
+  },
+
+
+  Eminem: {
+    image: "https://preview.redd.it/thoughts-on-eminem-v0-qsr34joea0cd1.jpeg?width=1080&crop=smart&auto=webp&s=0f0c28888bb9cf6b51afcdfdb6fd5d9980140089",
+    description: "no desc for now",
+  },
+
+
+  Elon_Musk: {
+    image: "https://external-preview.redd.it/elon-musks-biographer-calls-him-a-sociopath-after-auschwitz-v0-A8ur02Vr3WW9WiVumu3yOgm5SUagOYkuDy9zmWJy3JI.jpg?auto=webp&s=f8fc4124a19b04bf9eb9623f15d6f70a17951d8e",
+    description: "no desc for now",
+  },
+
+
+  Steve_Jobs: {
+    image: "https://external-preview.redd.it/sMSsAQGK5_sQJbPjCnAHmno9OkmexMp-yVuMCJZTIxw.jpg?auto=webp&s=4da4f7b4f55b9576e11016d84c0e13851aba8d0f",
+    description: "no desc for now",
+  },
+
+
+  PewDiePie: {
+    image: "https://preview.redd.it/what-would-be-the-first-thing-you-say-to-pewdiepie-if-you-v0-r6iwje6x6m9e1.jpeg?width=640&crop=smart&auto=webp&s=d7ac8451eaf49a18a5af40c01871ac1d9820b34b",
+    description: "no desc for now",
+  },
+
+
+  Michael_Jackson: {
+    image: "https://external-preview.redd.it/michael-jackson-was-500-million-in-debt-when-he-died-v0-Gl0ZkSNqjFt3xx3B77RLtxCwOZRq-bRNqd261Uw0SX4.jpg?auto=webp&s=9951d6607787a2881ef0d406b428c98f06dc9c6d",
+    description: "no desc for now",
+  },
+
+
+  Adolf_Hitler: {
+    image: "https://external-preview.redd.it/Hm0PoJRhLxy_pLe--JxeqtP8FZVcpyC1G-QZVExNa4U.jpg?auto=webp&s=5fb4d68896a55e3863ea42e08e14721cb7c4a78e",
+    description: "no desc for now",
+  }
+  ,
+
+
+};
+
+export function getdata() {
+  return celebrities;
+}
