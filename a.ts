09d4a4231563d1825f149f9e70fbf69fea007152
Jaitrika <141649577+Jaitrika@users.noreply.github.com>
2025-03-29 19:22:02 +0530

    Update and rename animaldata.ts to a.ts

---
+++ a.ts
@@ -0,0 +1,113 @@
+export const animals = {
+  dog: {
+    image: "https://preview.redd.it/our-best-friend-did-not-make-it-today-here-my-favorite-v0-mo9mg5s48oqe1.jpg?width=1080&crop=smart&auto=webp&s=e9e6e6bc9ca3c4993edcf254a9ec539a8c8cf4f2",
+    description: "The dog (Canis familiaris) is a domesticated descendant of the gray wolf, selectively bred by hunter-gatherers during the Late Pleistocene. It was the first species domesticated by humans and has adapted to thrive on a starch-rich diet. Dogs have been bred for behaviors, sensory abilities, and physical traits, resulting in diverse breeds. They possess strong senses of smell and hearing, though their vision is less acute than humans'. Dogs serve various roles, including hunting, herding, protection, companionship, therapy, and assisting law enforcement. They communicate through vocalizations, body language, and scent marking. Their deep bond with humans has earned them the title man's best friend. Dogs are the most popular pet in the U.S.",
+  },
+  elephant: {
+    image: "https://preview.redd.it/elephant-and-baby-v0-cac1658v0fpe1.jpeg?width=1080&crop=smart&auto=webp&s=398c159ee0bc598d204a69f262df16a78f0569b9",
+    description: "Elephants are the largest living land animals. Three living species are currently recognised: the African bush elephant (Loxodonta africana), the African forest elephant (L. cyclotis), and the Asian elephant (Elephas maximus). They are the only surviving members of the family Elephantidae and the order Proboscidea; extinct relatives include mammoths and mastodons. Distinctive features of elephants include a long proboscis called a trunk, tusks, large ear flaps, pillar-like legs, and tough but sensitive grey skin. The trunk is prehensile, bringing food and water to the mouth and grasping objects. Tusks, which are derived from the incisor teeth, serve both as weapons and as tools for moving objects and digging.",
+  },
+  lion: {
+    image: "https://preview.redd.it/kalu-postseason-detroit-african-lion-v0-ptv0xhpi6eie1.jpeg?auto=webp&s=ba7849456898fe031042cd1bd5aac31ba6d2cd3f",
+    description: "The lion (Panthera leo) is a large cat of the genus Panthera native to Africa and India. It has a muscular, broad-chested body; short, rounded head; round ears; and a hairy tuft at the end of its tail. It is sexually dimorphic; adult male lions are larger than females and have a prominent mane. Lions are apex predators that primarily hunt large ungulates, such as zebra, African buffalo, wildebeest, impala, and warthog. They live in groups called prides, consisting of a few adult males, related females, and cubs. They typically hunt in coordinated groups and stalk their chosen prey.",
+  },
+  tiger: {
+    image: "https://preview.redd.it/my-favourite-tiger-images-v0-etm0fegvm6de1.jpg?width=800&format=pjpg&auto=webp&s=a9238444ba55cebb3f839459e5a80ca2ea6b4f1f",
+    description: "The tiger (Panthera tigris) is the largest living cat species and a member of the genus Panthera. It is most recognisable for its dark vertical stripes on orange-brown fur with a lighter underside. Tigers are apex predators, primarily preying on ungulates such as deer and wild boar. They are territorial and generally solitary animals, often requiring large contiguous areas of habitat that support their prey. Tigers once ranged widely from the Eastern Anatolia Region to the Amur River basin, and in the south from the foothills of the Himalayas to Bali in the Sunda islands.",
+  },
+  penguin: {
+    image: "https://preview.redd.it/y8vpr0y07s801.png?auto=webp&s=13d9520f70cc59a155716dd53e68264d6669e233",
+    description: "Penguins are a group of aquatic flightless birds. They live almost exclusively in the Southern Hemisphere. Highly adapted for life in the water, penguins have countershaded dark and white plumage and flippers for swimming. Most penguins feed on krill, fish, squid and other forms of sea life which they catch while swimming underwater. They spend roughly half of their lives on land and the other half in the sea. The largest living species is the emperor penguin, which can reach heights of 100 cm and weights of 45 kg.",
+  },
+  giraffe: {
+    image: "https://preview.redd.it/thats-a-nice-pose-v0-l7jg005mbioe1.jpeg?auto=webp&s=05fbfe99e22fa4d9686abc018c6fcacd7e213fb9",
+    description: "The giraffe is a large African hoofed mammal belonging to the genus Giraffa. It is the tallest living terrestrial animal and the largest ruminant. Traditionally, giraffes were thought to be one species, with nine subspecies. However, recent genetic studies suggest that there may be up to six species. The giraffe's chief distinguishing characteristics are its extremely long neck and legs, its horn-like ossicones, and its distinctive coat patterns. It is classified under the family Giraffidae, along with its closest extant relative, the okapi.",
+  },
+  Cat: {
+    image: "https://i.redd.it/npoge7zncba21.jpg",
+    description: "no desc for now",
+  },
+
+  Horse: {
+    image: "https://i.redd.it/zjxl0xupyjzb1.jpg",
+    description: "no desc for now",
+  },
+
+  Cow: {
+    image: "https://i.redd.it/dsvik5qm4w8a1.jpg",
+    description: "no desc for now",
+  },
+
+  Rabbit: {
+    image: "https://i.redd.it/ermvosfyr7w71.jpg",
+    description: "no desc for now",
+  },
+
+  Hamster: {
+    image: "https://i.redd.it/8oxrtl6d0j551.jpg",
+    description: "no desc for now",
+  },
+
+  Zebra: {
+    image: "https://preview.redd.it/7fst7km9ho251.jpg?width=640&crop=smart&auto=webp&s=bd31a9fe8814e8f5ff5a799920656b7c488ced72",
+    description: "no desc for now",
+  },
+
+  Kangaroo: {
+    image: "https://external-preview.redd.it/7CUzOKL_XecI2GWT0JS_inefoeriBFpdRT5GCoWpo8k.jpg?auto=webp&s=bf304abfc21438c9174846273289cc1ae3a31258",
+    description: "no desc for now",
+  },
+
+  Panda: {
+    image: "https://preview.redd.it/all-giant-pandas-in-zoos-around-the-world-are-on-loan-from-v0-waravlpd7jib1.jpg?width=640&crop=smart&auto=webp&s=bb68427b60086b21775a41fb922d3e8491f50196",
+    description: "no desc for now",
+  },
+
+  Dolphin: {
+    image: "https://external-preview.redd.it/YUfEQvunQwS6vAnHW7bZvMyXZdITenUDjziUdbhmaGw.jpg?auto=webp&s=a79ca7574bc6d9cf3343bd832113154f2f9fe624",
+    description: "no desc for now",
+  }
+  ,
+
+  Shark: {
+    image: "https://i.redd.it/mtsvkg5vqrc71.jpg",
+    description: "no desc for now",
+  },
+
+  Frog: {
+    image: "https://i.redd.it/193zx4a0tnob1.jpg",
+    description: "no desc for now",
+  },
+
+  Snake: {
+    image: "https://i.redd.it/8wfimozol8591.jpg",
+    description: "no desc for now",
+  },
+
+  Crocodile: {
+    image: "https://preview.redd.it/8jyqlgiidey51.jpg?width=1080&crop=smart&auto=webp&s=b4f00dcbaa2daad41d3c629e540d0096f2300eef",
+    description: "no desc for now",
+  },
+
+  Eagle: {
+    image: "https://i.redd.it/rtg42b5aak471.jpg",
+    description: "no desc for now",
+  }
+  ,
+  Owl: {
+    image: "https://i.redd.it/v4505zum7vc71.jpg",
+    description: "no desc for now",
+  }
+  ,
+  Peacock: {
+    image: "https://preview.redd.it/peacocks-are-so-beautiful-but-i-also-find-them-really-scary-v0-fn737vx8l41d1.png?auto=webp&s=0baca6fb12a002fa9d03741e18dba53a138203da",
+    description: "no desc for now",
+  }
+
+  ,
+
+};
+
+export function getdata() {
+  return animals;
+}
